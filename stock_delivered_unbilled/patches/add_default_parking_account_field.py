3d7c5a9552988a25a9d48d5b3216822a762f75c0
Vignesh Sekar <vignesh@aerele.in>
2024-01-31 21:23:36 +0530

    Add after install script

--- stock_delivered_unbilled/patches/add_default_parking_account_field.py
+++ stock_delivered_unbilled/patches/add_default_parking_account_field.py
@@ -4,6 +4,7 @@ from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
 
 def execute():
 	create_parking_account_field()
+	create_disable_in_return_field()
 
 def create_parking_account_field():
 	custom_field = {
@@ -17,6 +18,21 @@ def create_parking_account_field():
 				ignore_user_permissions=1,
 				no_copy=1,
 			)
+		]
+	}
+	create_custom_fields(custom_field, ignore_validate=frappe.flags.in_patch, update=True)
+
+def create_disable_in_return_field():
+	custom_field = {
+		"Company": [
+			dict(
+				fieldname="disable_sdbnb_in_sr",
+				label="Disable Stock Delivered But Not Billed in Sales Return",
+				fieldtype="Check",
+				insert_after="stock_delivered_but_not_billed",
+				ignore_user_permissions=1,
+				no_copy=1,
+			),
 		]
 	}
 	create_custom_fields(custom_field, ignore_validate=frappe.flags.in_patch, update=True)
