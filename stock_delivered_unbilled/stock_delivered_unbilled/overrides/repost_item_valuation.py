8cb8d6f220b7f5b4348ff79e51f7bcb4a8b3e789
Vignesh Sekar <vignesh@aerele.in>
2024-02-08 01:28:27 +0530

    add default expense account

--- stock_delivered_unbilled/stock_delivered_unbilled/overrides/repost_item_valuation.py
+++ stock_delivered_unbilled/stock_delivered_unbilled/overrides/repost_item_valuation.py
@@ -107,6 +107,15 @@ def validate_expense_accoount(self,method):
 	stock_delivered_but_not_billed_account = None
 	stock_delivered_but_not_billed_account = frappe.db.get_value("Company", self.company, "stock_delivered_but_not_billed")
 
+
+	disable_sdbnb_in_sr = frappe.db.get_value("Company", self.company, "disable_sdbnb_in_sr")
+	default_expense_account = frappe.db.get_value("Company", self.company, "default_expense_account")
+
 	if stock_delivered_but_not_billed_account:
-		for item in self.items:
-			item.expense_account = stock_delivered_but_not_billed_account
+		if disable_sdbnb_in_sr and self.is_return:
+			if default_expense_account:
+				for item in self.items:
+					item.expense_account = default_expense_account
+		else:
+			for item in self.items:
+				item.expense_account = stock_delivered_but_not_billed_account
